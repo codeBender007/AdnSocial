f7532f403c25e5928d6f965b950749ecd8b50440
CodeBender <157215759+codeBender007@users.noreply.github.com>
2024-09-23 03:05:05 +0530

    Update LoginCard.jsx

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -39,7 +39,7 @@ export default function LoginCard() {
     const handleLogin = async () => {
         setLoading(true)
         try {
-            const res = await fetch("/api/users/login", {
+            const res = await fetch("https://mohdadnan.onrender.com/api/users/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
