15fb9d01a5d11037fa97fae1725d0069b4943be6
CodeBender <157215759+codeBender007@users.noreply.github.com>
2024-09-23 01:43:23 +0530

    files added

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useRecoilState } from 'recoil';
+import userAtom from '../atoms/userAtom';
+import useShowToast from './useShowToast';
+
+export default function useLogout() {
+
+    const [user ,setUser] = useRecoilState(userAtom);
+    const showToast = useShowToast();
+
+    const logout = async () => {
+        try {
+            const res = await fetch("/api/users/logout", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+
+            const data = await res.json();
+
+            if (data.error) {
+                showToast("Error", data.error, "error");
+                return;
+            }
+
+            // Remove user data and clear state
+            localStorage.removeItem("user-threads");
+            setUser(null);
+        } catch (err) {
+            console.error("Logout error:", err);
+            showToast("Error", err.message);
+        }
+    };
+    return logout;
+}
