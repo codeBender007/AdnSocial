31323ef03cb648ae163a1e031166fa8eef2dd751
CodeBender <157215759+codeBender007@users.noreply.github.com>
2024-09-25 17:42:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const connectDB = require("./db/connectDB")
 const dotenv = require('dotenv')
-// const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser")
 dotenv.config();
-// const app = express();
+const app = express();
 const { app, server } = require('./socket/socket')
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({extended:true}));
